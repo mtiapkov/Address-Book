b83bd3b97e8526d16ee800e85b17ec6a3d73fc33
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-24 10:09:01 -0700

    add assign/find/delete ID methods
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,42 @@
 // Business Logic for AddressBook
 function AddressBook() {
-  this.contacts = [];
-  this.currentID = 0;
+  this.contacts = [],
+  this.currentId = 0
 }
-AddressBook.prototype.addContact = function(contact){
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId; 
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }                          
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i <this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      delete this.contacts[i];
+      return true;
+    }
+  };
+  return false;
+}
+
+
+
+
 
 //Business Logic for Contacts
 function Contact (firstName, lastName, phoneNumber) {
